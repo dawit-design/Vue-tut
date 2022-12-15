672d0a406e0684fdc260dcd6c012d41db9dbfd0e
dawit-design <dawitawegchow@gmail.com>
2022-12-15 14:31:27 -0500

    working with data inside vue app

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,10 +1,21 @@
 const app = Vue.createApp({
     data(){
         return{
-            courseGoal: 'Finish the course!',
+            courseGoalA: 'Finish the vue course!',
+            courseGoalB: 'Master the course!',
             vueLink: 'https://vuejs.org/guide/quick-start.html#using-vue-from-cdn',
         };
     },
+    methods: {
+        outputGoal(){
+            const randNum = Math.random();
+            if(randNum < 0.5) {
+                return this.courseGoalA
+            }else{
+                return this.courseGoalB
+            }
+        }
+    }
 });
 
 
