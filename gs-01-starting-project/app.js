2e1e899ab1a82534506d288e54ceaf151fc94e0e
dawit-design <dawitawegchow@gmail.com>
2022-12-15 13:39:55 -0500

    created a simple to do app with vanilla js

---
+++ gs-01-starting-project/app.js
@@ -0,0 +1,13 @@
+const buttonEl = document.querySelector('button')
+const inputEl = document.querySelector('input')
+const listEl = document.querySelector('ul')
+
+function addGoal(){
+    const enteredVal = inputEl.value;
+    const listItemEl = document.createElement('li')
+    listItemEl.textContent = enteredVal
+    listEl.appendChild(listItemEl)
+    inputEl.value = '';
+}
+
+buttonEl.addEventListener('click', addGoal)
