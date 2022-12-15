b990be29edad949728c231f28875d2d4aefbee93
dawit-design <dawitawegchow@gmail.com>
2022-12-15 13:53:37 -0500

    recreate the to do app with vue

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,13 +1,28 @@
-const buttonEl = document.querySelector('button')
-const inputEl = document.querySelector('input')
-const listEl = document.querySelector('ul')
+Vue.createApp({
+    data(){
+        return{
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = '';
+        }
+    }
+}).mount('#app')
 
-function addGoal(){
-    const enteredVal = inputEl.value;
-    const listItemEl = document.createElement('li')
-    listItemEl.textContent = enteredVal
-    listEl.appendChild(listItemEl)
-    inputEl.value = '';
-}
+// const buttonEl = document.querySelector('button')
+// const inputEl = document.querySelector('input')
+// const listEl = document.querySelector('ul')
 
-buttonEl.addEventListener('click', addGoal)
+// function addGoal(){
+//     const enteredVal = inputEl.value;
+//     const listItemEl = document.createElement('li')
+//     listItemEl.textContent = enteredVal
+//     listEl.appendChild(listItemEl)
+//     inputEl.value = '';
+// }
+
+// buttonEl.addEventListener('click', addGoal)
