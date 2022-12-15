b1bbc2b0f019350f0e42bd55a4aae37408cc7378
dawit-design <dawitawegchow@gmail.com>
2022-12-15 15:54:53 -0500

    adding the native event object

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,14 +2,18 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: ''
     };
   },
   methods: {
-    add(){
-      this.counter++;
+    setName(event){
+      this.name = event.target.value
     },
-    reduce(){
-      this.counter = this.counter - 1;
+    add(num){
+      this.counter= this.counter + num;
+    },
+    reduce(num){
+      this.counter = this.counter - num;
     }
   }
 });
