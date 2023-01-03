0846f519955b6bf9954b8da3d5e3e4f45ccfa3eb
dawit-design <dawitawegchow@gmail.com>
2023-01-03 14:51:09 -0500

    added basic form validation

--- forms-01-starting-setup/src/components/TheForm.vue
+++ forms-01-starting-setup/src/components/TheForm.vue
@@ -1,8 +1,9 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div class="form-control" :class="{invalid: userNameValid === 'invalid'}">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" v-model="userName"/>
+      <input id="user-name" name="user-name" type="text" v-model.trim="userName" @blur="validateInput"/>
+      <p v-if="userNameValid === 'invalid'">Please enter a valid name!</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
@@ -64,7 +65,8 @@
       referrer:'wom',
       interest: [],
       how: null,
-      confirm: false
+      confirm: false,
+      userNameValid: 'pending',
     }
   },
   methods: {
@@ -77,7 +79,14 @@
       this.how = null;
       this.confirm = false;
 
-    }
+    },
+    validateInput(){
+      if(this.userName === ''){
+        this.userNameValid = 'invalid';
+      }else{
+        this.userNameValid = 'valid';
+      }
+    },
   }
  }
 </script>
@@ -93,6 +102,13 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+}
+
+.form-control.invalid input{
+  border-color: red;
+}
+.form-control.invalid label {
+  color: red;
 }
 
 label {
